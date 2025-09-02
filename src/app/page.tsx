0f0315a3b1dc5acb4fad82fcf95572043a4f24e9
Artemisa <a.nuri@codevider.com>
2025-09-02 16:48:46 +0200

    removed loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
 import CaseStudiesSection from "./components/CaseStudies";
 import Faq from "./components/Faq";
 import Hero from "./components/HeroSection";
@@ -14,42 +12,23 @@ import WorldMapDemo from "./components/WorldMap";
 import ScrollReveal from "./components/ScrollReveal";
 import Metrics from "./components/Metrics";
 import Contact from './components/CTA';
-import Loader from './components/Loader/loader';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <div className="min-h-dvh bg-black">
-      <AnimatePresence>
-        {/* The Loader remains the same, it will exit gracefully */}
-        {isLoading && (
-          <motion.div key="loader">
-            <Loader onLoadingComplete={() => setIsLoading(false)} />
-          </motion.div>
-        )}
-      </AnimatePresence>
-      
-
-      {!isLoading && (
-        <>
-          <Header />
-          <main id="home">
-            <Hero /> 
-            
-            {/* The rest of your content remains the same */}
-            <ScrollReveal delay={0.1}><Metrics /></ScrollReveal>
-            <ScrollReveal delay={0.1}><ServicesPage /></ScrollReveal>
-            <ScrollReveal delay={0.1}><Industries /></ScrollReveal>
-            <ScrollReveal delay={0.1}><Outsource /></ScrollReveal>
-            <ScrollReveal delay={0.1}><WorldMapDemo /></ScrollReveal>
-            <ScrollReveal delay={0.1}><CaseStudiesSection /></ScrollReveal>
-            <ScrollReveal delay={0.1}><TestimonialsPage /></ScrollReveal>
-            <ScrollReveal delay={0.1}><Faq /></ScrollReveal>
-            <Contact />
-          </main>
-        </>
-      )}
+      <Header />
+      <main id="home">
+        <Hero />
+        <ScrollReveal delay={0.1}><Metrics /></ScrollReveal>
+        <ScrollReveal delay={0.1}><ServicesPage /></ScrollReveal>
+        <ScrollReveal delay={0.1}><Industries /></ScrollReveal>
+        <ScrollReveal delay={0.1}><Outsource /></ScrollReveal>
+        <ScrollReveal delay={0.1}><WorldMapDemo /></ScrollReveal>
+        <ScrollReveal delay={0.1}><CaseStudiesSection /></ScrollReveal>
+        <ScrollReveal delay={0.1}><TestimonialsPage /></ScrollReveal>
+        <ScrollReveal delay={0.1}><Faq /></ScrollReveal>
+        <Contact />
+      </main>
     </div>
   );
 }
