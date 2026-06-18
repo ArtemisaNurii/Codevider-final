464ff4768482fa32aa3852d66c69473e8fa713f8
Vasjan Çupri <v.cupri@codevider.com>
2026-06-18 17:27:19 +0200

    codevider2.0

--- src/constants/endpoint.ts
+++
@@ -1,5 +0,0 @@
-export const RECRUIT_JOBS_ENDPOINT = 'recruit/jobs/open'
-export const LEAD_CONTACTS_ENDPOINT = 'leads/contact-submissions'
-export const APPLY_JOB_ENDPOINT = 'recruit/website-applications'
-export const RESUME_AI="recruit/candidate/job-application"
-export const RESUME_AI_UPLOAD="recruit/candidate/job-application/upload"
